2ab7b46208feb6465c2863dad7a97b53adbffb62
Juha Yoon <ijooha16@gmail.com>
2025-07-22 15:59:20 +0900

    feature: mypage

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+"use client";
+
+import useAuthStore from "@/stores/use-auth-store";
+import { useGetUserQuery } from "@/tanstack/queries/get-user-query";
+import Image from "next/image";
+import React from "react";
 
 const MyPage = () => {
-  return (
-    <div>MyPage</div>
-  )
-}
+  const { userId } = useAuthStore();
+  const { data } = useGetUserQuery(userId);
+  const { email, nickname, avatar_url: image } = data;
 
-export default MyPage
+  return (
+    <div>
+      {data && (
+        <>
+          <div>{email}</div>
+          <div>{nickname}</div>
+          <Image src={image} alt="profile image" width={40} height={40} />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default MyPage;
