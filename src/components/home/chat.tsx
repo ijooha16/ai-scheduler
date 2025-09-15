24c6f0056dceba18b6eb0f024fb463dfcba8c48c
Juha Yoon <ijooha16@gmail.com>
2025-09-15 18:21:58 +0900

    feature/비로그인 사용자 로그인 화면으로

--- src/components/home/chat.tsx
+++ src/components/home/chat.tsx
@@ -1,6 +1,7 @@
 import { Dispatch, FormEvent, SetStateAction } from "react";
 import Button from "../common/button";
 import BoxContainer from "../layout/box-container";
+import { useRouter } from "next/navigation";
 
 const Chat = ({
   inputValue,
@@ -8,12 +9,23 @@ const Chat = ({
   onSubmitHandler,
   disabled,
 }: ChatProps) => {
+  const router = useRouter();
   const placeholder = disabled
     ? "로그인 하고 사용해보세요!"
     : "원하는 목표를 적어보세요. 구체적일수록 좋아요!";
 
   return (
     <BoxContainer className="h-36 w-full max-w-3xl shrink-0 bg-white">
+      {disabled && (
+        <div
+          className="absolute h-full w-full"
+          onClick={() => {
+            if (disabled) {
+              router.push("/login");
+            }
+          }}
+        />
+      )}
       <form
         onSubmit={onSubmitHandler}
         className="flex h-full flex-col items-end justify-between gap-8"
